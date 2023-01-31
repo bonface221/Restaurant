d240d182fd95700cc602de1ed8d0d52441f42849
bonface221 <mainaboniface221@gmail.com>
2023-02-01 01:26:06 +0300

    ft:finished on thebackend

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from .filters import MyFilter
 from .models import Restaurant
 from .serializers import RestaurantSerializer
 
-# Create your views here.
+
+
 class RestaurantViewSet(ModelViewSet):
     queryset = Restaurant.objects.all()
-    serializer_class= RestaurantSerializer
-    
+    serializer_class = RestaurantSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = MyFilter
+    search_fields = ['navn']
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+		<div className="App">
+			<h2 className="text-center pt-5">Restaurant Search</h2>
+			<div className="form d-flex justify-content-center">
+				<form className="row mt-5">
+					<div className="col-md-6">
+						<label className="form-label" htmlFor="poststed">
+							Enter Country search
+						</label>
+						<input className="form-control" id="poststed" type="text" />
+					</div>
+					<div className="col-md-6">
+						<label className="form-label" htmlFor="poststed">
+							Search for a restaurant
+						</label>
+						<input className="form-control" id="poststed" type="text" />
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App
