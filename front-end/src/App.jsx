a198c1f238f34b2422591883f9f389df48c1ae30
bonface221 <mainaboniface221@gmail.com>
2023-02-04 17:01:24 +0300

    ft:optimized styling

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,41 +5,42 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Card from "./components/card/Card";
 import Country from "./components/countryBased/country";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
-
 	const url = "https://hotell.difi.no/api/json/mattilsynet/smilefjes/tilsyn?";
 
-	useEffect(() => {
-		setInterval(() => {
-			let axiData = [];
+	// useEffect(() => {
+	// 	setInterval(() => {
+	// 		let axiData = [];
 
-			axios.get(url).then(({ data: { entries, pages } }) => {
-				axiData = [...axiData, ...entries];
+	// 		axios.get(url).then(({ data: { entries, pages } }) => {
+	// 			axiData = [...axiData, ...entries];
 
-				let x = 2;
+	// 			let x = 2;
 
-				let timer = setInterval(() => {
-					if (x === pages + 1) {
-						clearInterval(timer);
-						communicateWithBackend(axiData);
-					}
-					(async () => {
-						let response = await myPromise(x);
-						axiData = [...axiData, ...response];
-					})();
-					x++;
-				}, 2000);
-			});
-		},86400000)
-		
-	}, []);
+	// 			let timer = setInterval(() => {
+	// 				if (x === pages + 1) {
+	// 					clearInterval(timer);
+	// 					communicateWithBackend(axiData);
+	// 				}
+	// 				(async () => {
+	// 					let response = await myPromise(x);
+	// 					axiData = [...axiData, ...response];
+	// 				})();
+	// 				x++;
+	// 			}, 2000);
+	// 		});
+	// 	},86400000)
+
+	// }, []);
 
 	function communicateWithBackend(arr) {
 		arr.forEach((d) => {
-			axios.post("http://127.0.0.1:8000/restaurants/", d)
-			.then((res)=>console.log(res))
-		})
+			axios
+				.post("http://127.0.0.1:8000/restaurants/", d)
+				.then((res) => console.log(res));
+		});
 	}
 
 	function myPromise(x) {
@@ -52,13 +53,12 @@ function App() {
 
 	return (
 		<div className="App">
-			
+			<Navbar />
 			<Routes>
-				<Route path="/" element={<Home/>} />
-				<Route path="/restaurant/:id" element={<Card/>} />
-				<Route path="/restaurant/country/:country" element={<Country/>} />
+				<Route path="/" element={<Home />} />
+				<Route path="/restaurant/:id" element={<Card />} />
+				<Route path="/restaurant/country/:country" element={<Country />} />
 			</Routes>
-		
 		</div>
 	);
 }
--- front-end/src/components/card/Card.jsx
+++ front-end/src/components/card/Card.jsx
@@ -34,46 +34,51 @@ function Card() {
 	let url = "http://127.0.0.1:8000/restaurants/";
 
 	useEffect(() => {
-		axios
-			.get(`${url}${id}/`)
-			.then((res) => setData((prev) => {
-				console.log(prev)
-				console.log(res.data)
+		axios.get(`${url}${id}/`).then((res) =>
+			setData((prev) => {
+				console.log(prev);
+				console.log(res.data);
 				return {
 					...prev,
-					...res.data
-				}
-			}));
-	},[]);
+					...res.data,
+				};
+			})
+		);
+	}, []);
 	return (
 		<div className="Restraurant-detail pt-2 mb-3 container">
-			<button onClick={()=>window.location.href = '/'} className="btn btn-primary btn-sm">go back</button>
 			{data.navn && (
 				<div className="">
-					<h3 className="my-2">Details of {data.navn}</h3>
-					<p>poststed - {data.poststed}</p>
-					<p>sakref - {data.sakref}</p>
-					<p>tilsynsbesoektype - {data.tilsynsbesoektype}</p>
-					<p>orgnummer - {data.orgnummer}</p>
-					<p>postnr - {data.postnr}</p>
-					<p>dato - {data.dato}</p>
-					<p>tema1_no - {data.tema1_no}</p>
-					<p>tema3_nn - {data.tema3_nn}</p>
-					<p>tema1_nn - {data.tema1_nn}</p>
-					<p>tema3_no - {data.tema3_no}</p>
-					<p>tilsynid - {data.tilsynid}</p>
-					<p>adrlinje1 - {data.adrlinje1}</p>
-					<p>karakter1 - {data.karakter1}</p>
-					<p>karakter2 - {data.karakter2}</p>
-					<p>karakter3 - {data.karakter3}</p>
-					<p>karakter4 - {data.karakter4}</p>
-					<p>adrlinje2 - {data.adrlinje2}</p>
-					<p>tema4_no - {data.tema4_no}</p>
-					<p>tema4_nn - {data.tema4_nn}</p>
-					<p>tema2_no - {data.tema2_no}</p>
-					<p>status - {data.status}</p>
+					<h3 className="my-2 fs-2">Details of {data.navn}</h3>
+					<div className="mt-3 row">
+						<div className="col-md-6">
+							<p>poststed - {data.poststed}</p>
+							<p>sakref - {data.sakref}</p>
+							<p>tilsynsbesoektype - {data.tilsynsbesoektype}</p>
+							<p>orgnummer - {data.orgnummer}</p>
+							<p>postnr - {data.postnr}</p>
+							<p>dato - {data.dato}</p>
+							<p>tema1_no - {data.tema1_no}</p>
+							<p>tema3_nn - {data.tema3_nn}</p>
+							<p>tema1_nn - {data.tema1_nn}</p>
+							<p>tema3_no - {data.tema3_no}</p>
+							<p>tilsynid - {data.tilsynid}</p>
+						</div>
+						<div className="col-md-6">
+							<p>adrlinje1 - {data.adrlinje1}</p>
+							<p>karakter1 - {data.karakter1}</p>
+							<p>karakter2 - {data.karakter2}</p>
+							<p>karakter3 - {data.karakter3}</p>
+							<p>karakter4 - {data.karakter4}</p>
+							<p>adrlinje2 - {data.adrlinje2}</p>
+							<p>tema4_no - {data.tema4_no}</p>
+							<p>tema4_nn - {data.tema4_nn}</p>
+							<p>tema2_no - {data.tema2_no}</p>
+							<p>status - {data.status}</p>
 
-					<p>total_karakter - {data.total_karakter}</p>
+							<p>total_karakter - {data.total_karakter}</p>
+						</div>
+					</div>
 				</div>
 			)}
 		</div>
